11354f13ffd3a4e397f93029bbd9fab3f454d968
xuantruongcox <truongcool999@gmail.com>
2023-03-25 08:54:12 +0700

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,31 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+})
 module.exports = {
   siteMetadata: {
     title: `test-multiligual`,
-    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: '@kontent-ai/gatsby-source',
+      options: {
+        projectId: process.env.KONTENT_ACCESS,
+        languageCodenames: [
+          'default'
+        ]
+      }
+    }, {
+      resolve: 'gatsby-plugin-i18n',
+      options: {
+        langKeyDefault: 'en',
+        langKeyForNull: 'en',
+        prefixDefault: false,
+        useLangKeyLayout: false
+      }
+    }
+  ],
 }
